b866be602ecd13d0a1db64db59b3ad849ffe3d2f
양희진 <yanghj1@flow.team>
2026-04-14 11:05:00 +0900

    refactor: auth모듈 global 설정

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { UserModule } from '../user/user.module';
 import { ClerkAuthGuard } from './clerk-auth.guard';
 import { AuthService } from './auth.service';
 
+@Global()
 @Module({
   imports: [UserModule],
   providers: [
